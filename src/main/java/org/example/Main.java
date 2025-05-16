ec1aae92412df7692fbc6d96947f9aed17345cad
SokolDZR <33631961+SokolDZR@users.noreply.github.com>
2025-05-17 01:12:44 +0300

    not ready

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.List;
 
-@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
--- src/main/java/org/example/dao/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.dao;
-
-import org.example.userService.model.Employee;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmployeeRepository extends CrudRepository<Employee, Long>{
-}
